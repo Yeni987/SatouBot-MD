f47e2e02503cad80a627ede4f8021ca234d4fe0c
LegnaOfc <Beatrizmanjarreztorres6@gmail.com>
2024-12-07 12:45:49 -0700

    Actualizar owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -1,7 +1,7 @@
 
 const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
-        conn.sendMessage(m.chat, {text: `🚩 *@${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
+        conn.sendMessage(m.chat, {text: `🍭 *@${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
       global.db.data.users[m.sender].money = Infinity;
     global.db.data.users[m.sender].estrellas = Infinity;
   global.db.data.users[m.sender].level = Infinity;
