63fffa2f714d9e4a6e96f61f2a0f06f10901b675
LegnaOfc <Beatrizmanjarreztorres6@gmail.com>
2024-12-07 18:24:12 -0700

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,8 +6,8 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     return;
   }
   const pesan = args.join` `;
-  const colombia = `💌 *Mensaje:* ${pesan}`;
-  let teks = `💥 *INVOCANDO GRUPO*\n${colombia}\n\n💖 *Tags:*\n`;
+  const colombia = `🌸 *Mensaje:* ${pesan}`;
+  let teks = `🍭 *INVOCANDO GRUPO*\n${colombia}\n\n💖 *Tags:*\n`;
   for (const mem of participants) {
     teks += `@${mem.id.split('@')[0]}\n`;
   }
