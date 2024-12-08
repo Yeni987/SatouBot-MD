14a341a0135eae2909ec014dce7b62e70624fd2f
LegnaOfc <Beatrizmanjarreztorres6@gmail.com>
2024-12-07 22:56:15 -0700

    Actualizar fun-casino.js

--- plugins/fun-casino.js
+++ plugins/fun-casino.js
@@ -25,13 +25,13 @@ if (args.length < 1) return conn.reply(m.chat, '🍭 Ingresa la cantidad de ' + 
 if (user.estrellas >= count * 1) {
 user.estrellas -= count * 1
 if (Aku > Kamu) {
-conn.reply(m.chat, '`🌸 Veamos que numeros tienen!`\n\n'+ `➠ *IanBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🥖 Panes.`.trim(), m, rcanal)
+conn.reply(m.chat, '`🌸 Veamos que numeros tienen!`\n\n'+ `➠ *SatouBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🥖 Panes.`.trim(), m, rcanal)
 } else if (Aku < Kamu) {
 user.estrellas += count * 2
-conn.reply(m.chat, '`🌸 Veamos que numeros tienen!`\n\n'+ `➠ *IanBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🥖 Panes.`.trim(), m, rcanal)
+conn.reply(m.chat, '`🌸 Veamos que numeros tienen!`\n\n'+ `➠ *SatouBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🥖 Panes.`.trim(), m, rcanal)
 } else {
 user.estrellas += count * 1
-conn.reply(m.chat, '`🌸 Veamos que numeros tienen!`\n\n'+ `➠ *ianBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🥖 Panes.`.trim(), m, rcanal)}
+conn.reply(m.chat, '`🌸 Veamos que numeros tienen!`\n\n'+ `➠ *SatouBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🥖 Panes.`.trim(), m, rcanal)}
 } else conn.reply(m.chat, `No tienes *${formatNumber(count)} 🥖 Panes* para apostar!`.trim(), m, rcanal)}
 
 handler.help = ['apostar *<cantidad>*']
