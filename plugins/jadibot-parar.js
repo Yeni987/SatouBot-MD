e91cb88cfb062aba205a42f56d1c1cb127b2dc88
LegnaOfc <Beatrizmanjarreztorres6@gmail.com>
2024-12-09 01:25:54 -0700

    Actualizar jadibot-parar.js

--- plugins/jadibot-parar.js
+++ plugins/jadibot-parar.js
@@ -1,6 +1,6 @@
 
 let handler  = async (m, { conn }) => {
-if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, `❁ El Bot Principal No Se Puede Apagar`, m, rcanal, )
+if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, `❁ El bot principal no se puede apagar`, m, rcanal, )
 else {
 await conn.reply(m.chat, `🍭 Subbot Desactivado`, m, rcanal, )
 conn.ws.close()
