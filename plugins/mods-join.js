b4df3c779863b831ef700c5feef076f47a2649b3
LegnaOfc <Beatrizmanjarreztorres6@gmail.com>
2024-12-09 01:35:10 -0700

    Actualizar mods-join.js

--- plugins/mods-join.js
+++ plugins/mods-join.js
@@ -3,7 +3,7 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 
-if (!text) return m.reply(`🌸 Ingresa el enlace del grupo para unirme mi amor^^.`)
+if (!text) return m.reply(`❀ Ingresa el enlace del grupo para unirme mi amor^^.`)
 let [_, code] = text.match(linkRegex) || []
 if (!code) return m.reply('🌺 Enlace invalido.')
 let res = await conn.groupAcceptInvite(code)
