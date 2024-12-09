bb2a25a734d1d013c6e10af34db49ee304bcc933
LegnaOfc <Beatrizmanjarreztorres6@gmail.com>
2024-12-09 01:09:28 -0700

    Actualizar fun-acertijos.js

--- plugins/fun-acertijos.js
+++ plugins/fun-acertijos.js
@@ -13,16 +13,16 @@ const handler = async (m, {conn, usedPrefix}) => {
   const _clue = json.response;
   const clue = _clue.replace(/[A-Za-z]/g, '_');
   const caption = `
-ⷮ🍄 *ACERTIJOS*
+ⷮ🌺 *ACERTIJOS*
 ✨️ *${json.question}*
 
 ⏱️ *Tiempo:* ${(timeout / 1000).toFixed(2)} Segundos
-🌸 *Premio:* *+${poin}* Centavos 🪙`.trim();
+❀ *Premio:* *+${poin}* Centavos 🪙`.trim();
   conn.tekateki[id] = [
     await conn.reply(m.chat, caption, m), json,
     poin,
     setTimeout(async () => {
-      if (conn.tekateki[id]) await conn.reply(m.chat, `🌸 Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0]);
+      if (conn.tekateki[id]) await conn.reply(m.chat, `❀ Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0]);
       delete conn.tekateki[id];
     }, timeout)];
 };
