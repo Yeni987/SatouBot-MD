80c79de76e1976c221aa9c60e95a482386fed6aa
LegnaOfc <Beatrizmanjarreztorres6@gmail.com>
2024-12-09 01:13:14 -0700

    Actualizar gc-encuesta.js

--- plugins/gc-encuesta.js
+++ plugins/gc-encuesta.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-if (!args[0]) throw `❀ *Ingrese un texto para iniciar la escuesta.*\n\nℹ️ Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
-if (!text.includes('|')) throw  `❀ Separe las encuestas con *|* \n\nℹ️ Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!args[0]) throw `❀ *Ingrese un texto para iniciar la escuesta.*\n\nℹ️ Ejemplo ➩ \n*${usedPrefix + command}* texto|texto2...`
+if (!text.includes('|')) throw  `❀ Separe las encuestas con *|* \n\nℹ️ Ejemplo ➩ \n*${usedPrefix + command}* texto|texto2...`
 let a = []
 let b = text.split('|')
 for (let c = 0; c < b.length; c++) {
