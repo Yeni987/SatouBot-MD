d4047de48adc0eb32be9aed7fb45022e3c12e8da
LegnaOfc <Beatrizmanjarreztorres6@gmail.com>
2024-12-09 00:47:50 -0700

    Actualizar descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) {
-        throw m.reply(`*🌸 Ejemplo: ${usedPrefix + command}* https://vm.tiktok.com/ZMhAk8tLx/`);
+        throw m.reply(`*🍭 Ejemplo ➩ ${usedPrefix + command}* https://vm.tiktok.com/ZMhAk8tLx/`);
     }
 
     try {
